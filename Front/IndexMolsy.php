43c8ceefb58cb9711bc400f574a140fce28821dc
catyybe <errocatalina08@gmail.com>
2025-07-16 08:14:05 -0300

    Update IndexMolsy.php
    Mostramos la funcion footer en el index principal, agregue el main style para que el footer se mantenga siempre abajo, osea para que sea empujado para abajo. Lo que va dentro de ese echo va a ser lo que va a ir solo en la pagina principal, en la de inicio, osea la idea de los productos, ofertas y banners, pero el footer y el header van en todas las paginas, por eso son dos funciones separadas.

--- Front/IndexMolsy.php
+++ Front/IndexMolsy.php
@@ -1,5 +1,10 @@
 <?php
     include '../Logica/Metodos.php';
     echo oheader();
+
     echo menuhamburguesa();
+ echo '<main style="min-height: 60vh;">'; // Contenido de la pagina principal en indexmolsy  // 
+    echo '</main>';
+
+    echo ofooter();
 ?>
