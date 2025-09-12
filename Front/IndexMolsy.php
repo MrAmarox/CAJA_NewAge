d46f4a8d8854c3051f1b673c61918ce6db788dc9
catyybe <errocatalina08@gmail.com>
2025-09-12 00:30:09 -0300

    Puro estilo nomas y agregue unas cards al index

--- Front/IndexMolsy.php
+++ Front/IndexMolsy.php
@@ -2,7 +2,7 @@
 include_once '../Logica/Metodos.php';
 include_once '../Logica/producto.php';
 echo oheader();  // Este ya incluye <html>, <head> y <body>
-echo menuhamburguesa();
+//echo menuhamburguesa();
 
 echo '
 <div class="banner-calzacele">
@@ -12,6 +12,32 @@ echo '
     </div>
 </div>';
 
+//Cards
+echo '
+<div class="productos-container">
+<div class="producto-card">
+    <img src="Img\PolleraLu.jpg">
+    <h3> Pollera Lu </h3>
+    <p> $390 </p>
+    <button class="btn-agregar"><i class="bi bi-cart-plus"></i> Agregar al carrito</button>
+</div>';
+echo '
+<div class="producto-card">
+    <img src="Img\ShortVal.jpg">
+    <h3> Short Val </h3>
+    <p> $320 </p>
+    <button class="btn-agregar"><i class="bi bi-cart-plus"></i> Agregar al carrito</button>
+</div>';
+echo '
+<div class="producto-card">
+    <img src="Img\ShortBatik.jpg">
+    <h3> Short Batik </h3>
+    <p> $390 </p>
+    <button class="btn-agregar"><i class="bi bi-cart-plus"></i> Agregar al carrito</button>
+</div>
+</div>';
+
+echo '<main style="min-height: 0vh;"></main>';
 
 echo'
 <div class="banner-Socks">
@@ -20,6 +46,6 @@ echo'
     </div>
 </div>';
 
-echo '<main style="min-height: 20vh;"></main>';
+echo '<main style="min-height: 0vh;"></main>';
 echo ofooter(); // </body> & </html>
 ?>
