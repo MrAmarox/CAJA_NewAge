47b41547c336fd30b3b916a5c5771758680b960a
MrAmarox <mramarox@hotmail.com>
2025-10-08 10:49:17 -0300

    l

---
+++ Front/Checkout.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../Logica/Metodos.php';
+include_once '../Logica/Carrito.php';
+    echo oheader();
+    ?>
+    <div class='producto-container'>
+        <div>
+            <?php
+            echo carrtab();
+            ?>
+        </div>
+
+    </div>
+
+    <?php
+?>
